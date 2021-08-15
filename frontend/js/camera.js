3eb17b3f5b83bd5ef556e5356f860e8e5b9fd0c6
raneem moualla <raneemmoualla@gmail.com>
2021-08-16 00:23:35 +0200

    mise en place des cards

--- frontend/js/camera.js
+++ frontend/js/camera.js
@@ -1,6 +1,13 @@
 
-
-
+class Cameras {
+    constructor(product) {
+        this.name = product.name;
+        this.imageUrl = product.imageUrl;
+        this.price = product.price;
+        this.id = product._id;
+        this.description = product.description;
+    }
+}
 
 
 
--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-//selectione la balise html dans le dom
-const container = document.getElementById('container');
+/*creation cards */
 
-//APPEL API AVEC FETCH
+let cardsProduct = document.getElementById("cardsProduct");
+
+let container = document.getElementById("container");
+
 fetch("http://localhost:3000/api/cameras")
-    .then(response => response.json())  
-    .then(function (listeProduct)
-    {
-    console.log(listeProduct);
-   
+.then (function(response){
+    return response.json()
+})  
+
+.then( function(listeProduct){
+    console.log(listeProduct);/*attention*/
+  for(let product of listeProduct){
+      let camera = new Cameras(product)
+    function display(product){
+        container.innerHTML +=
+    `<article id="cardsProduct" class="produit">
+        <img src=${product.imageUrl} alt="" />
+      
+        <h2> ${product.name}</h2>
+        <p>${product.price/100}€</p>
+      
+        <p>${product.description}</p>
         
-    }
-    )  
-    
-   
+        <a id="clicks" href="/frontend/pages/produit.html"> En savoir plus</a>
+    </article>`
+ }
+ display(camera);
+  }
+
+  });
+
+
